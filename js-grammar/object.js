0aec1b8cebf18caff43f990410e49e5ad68b994c
suzuki84g <suzuki.84g@gmail.com>
2020-04-14 16:40:27 +0900

    コンソールログに出力

--- js-grammar/object.js
+++ js-grammar/object.js
@@ -1,5 +1,15 @@
-if (confirm('OKを押して10秒だと思ったら何れかのキーを押して下さい')) {
-    // TODO:スタート処理
+var startTime = null;
+
+function start() {
+    startTime = Date.now();
+    document.body.onkeydown = stop;
     console.log('スタートしました');
+}
+function stop() {
+    console.log('ストップしました')
+}
+
+if (confirm('OKを押して10秒だと思ったら何れかのキーを押して下さい')) {
+    start();
 }
 
