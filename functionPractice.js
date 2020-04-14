763105c23ab900b8a5432696f0dc828b266f7bac
suzuki84g <suzuki.84g@gmail.com>
2020-04-14 15:04:28 +0900

    11 練習問題完了。難しい。

--- functionPractice.js
+++ functionPractice.js
@@ -3,11 +3,11 @@
 // 今回は、どのような半径に対しても面積を求められる関数を作りましょう。そして、その関数を用いて半径 5cm, 10cm, 15cm の円の面積を HTML に出力するプログラムを書いてみましょう。
 // （円の面積は 半径 × 半径 × 3.14 で求められます）。
 
-const radius = 15  // 半径を入力
-
-function circleCalc() {
-    var area = radius * radius * 3.14;
-    document.getElementById('birth-time').innerText = '半径' + radius + 'の円の面積は' + area + 'です。';
+function circleCalc(radius) {  // 引数radiusに半径を入力する
+    var area = radius * radius * 3.14;  // 計算
+    return area;
 }
 
-setInterval(circleCalc, 50);
+document.write('<p>半径5cmの円の面積は' + circleCalc(5) + 'です。</p>')
+document.write('<p>半径10cmの円の面積は' + circleCalc(10) + 'です。</p>')
+document.write('<p>半径15cmの円の面積は' + circleCalc(15) + 'です。</p>')
