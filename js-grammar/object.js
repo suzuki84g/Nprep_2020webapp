e47a7c7705540a8dd7823ab9c404764d1ddfc857
suzuki84g <suzuki.84g@gmail.com>
2020-04-14 16:44:59 +0900

    オブジェクト表現を使わない記載

--- js-grammar/object.js
+++ js-grammar/object.js
@@ -1,12 +1,21 @@
 var startTime = null;
+var displayArea = document.getElementById('display-area');
 
 function start() {
     startTime = Date.now();
     document.body.onkeydown = stop;
-    console.log('スタートしました');
+    // console.log('スタートしました');
 }
+
 function stop() {
-    console.log('ストップしました')
+    var currentTime = Date.now();
+    var seconds = (currentTime - startTime) / 1000;
+    if (9.5 <= seconds && seconds <= 10.5) {
+        displayArea.innerText = seconds + '秒でした。すごい。';
+    } else {
+        displayArea.innerText = seconds + '秒でした。残念。';
+    }
+    // console.log('ストップしました')
 }
 
 if (confirm('OKを押して10秒だと思ったら何れかのキーを押して下さい')) {
