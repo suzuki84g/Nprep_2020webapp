552f9ee40dfc096182bf9ef8e15b97175fdb1248
suzuki84g <suzuki.84g@gmail.com>
2020-04-14 11:23:10 +0900

    練習問題が出来た

--- js-grammar/loop.js
+++ js-grammar/loop.js
@@ -1,3 +1,12 @@
 for (var i = 1; i <= 100000; i++) {
-    document.write(i + ' ');
+    // if (i % 3 === 0 && i % 5 === 0) { // %15で良かった
+    if (i % 15 === 0) {
+        document.write('FizzBuzz' + ' ');
+    } else if (i % 3 === 0) {
+        document.write('Fizz' + ' ');
+    } else if (i % 5 === 0) {
+        document.write('Buzz' + ' ');
+    } else {
+        document.write(i + ' ');
+    }
 }
