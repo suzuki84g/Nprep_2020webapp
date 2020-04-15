d960a68fbf3a8e914d127b21213639b557ef1b0b
suzuki84g <suzuki.84g@gmail.com>
2020-04-15 12:09:50 +0900

    まとめまで完了

--- css-study/animation.js
+++ css-study/animation.js
@@ -3,6 +3,12 @@ var degree = 0;
 
 function rotateHeader() {
     degree = degree + 6;
+    degree = degree % 360;
+    if ((0 <= degree && degree < 90) || (270 <= degree && degree < 360)) {
+        header.className = 'face';
+    } else {
+        header.className = 'back';
+    }
     header.style.transform = 'rotateX(' + degree + 'deg)';
 }
 
