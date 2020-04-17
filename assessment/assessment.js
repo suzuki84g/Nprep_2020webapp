6339f5cb03f9a20be786ac35e0b7399e97645c35
suzuki84g <suzuki.84g@gmail.com>
2020-04-17 19:22:21 +0900

    診断ボタン押下時の無名関数を記述

--- assessment/assessment.js
+++ assessment/assessment.js
@@ -1,5 +1,16 @@
 'use strict';  // 厳格モード
 
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+assessmentButton.onclick = function() {
+    console.log('ボタンが押されました');
+    // #TODO:診断結果表示エリアの作成
+    // #TODO:ツイートエリアの作成
+}
+
 const answers = [
     '{userName}のいいところは声です。{userName} の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
