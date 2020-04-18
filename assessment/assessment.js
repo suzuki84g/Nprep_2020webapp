5bd6afd8c9c1942d0295c71432b661a389397a0e
suzuki84g <suzuki.84g@gmail.com>
2020-04-18 19:28:55 +0900

    18 練習問題完了

--- assessment/assessment.js
+++ assessment/assessment.js
@@ -54,6 +54,12 @@ assessmentButton.onclick = () => {
     tweetDivided.appendChild(script);
 };
 
+userNameInput.onkeydown = event => {
+    if (event.key === 'Enter') {
+        assessmentButton.onclick();
+    }
+};
+
 const answers = [
     '{userName}のいいところは声です。{userName} の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
