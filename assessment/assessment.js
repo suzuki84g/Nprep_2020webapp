5e03da4134e38b5406e7719918d2923c8094e21c
suzuki84g <suzuki.84g@gmail.com>
2020-04-18 13:18:38 +0900

    ボタンのデザインは変わるが診断結果を引用しない

--- assessment/assessment.js
+++ assessment/assessment.js
@@ -35,8 +35,23 @@ assessmentButton.onclick = () => {
     paragraph.innerText = result;
     resultDivided.appendChild(paragraph);
 
-    // #TODO:ツイートエリアの作成
+    // ツイートエリアの作成
     removeAllChildren(tweetDivided);
+    const anchor = document.createElement('a');
+    const hrefValue =
+        'https://twitter.com/intent/tweet?button_hashtag=' +
+        encodeURIComponent('あなたのいいところ') +
+        '&ref_src=twsrc%5Etfw';
+    anchor.setAttribute('href', hrefValue);
+    anchor.className = 'twitter-hashtag-button';
+    anchor.setAttribute('date-text', result);
+    anchor.innerText = 'Tweet #あなたのいいところ';
+    tweetDivided.appendChild(anchor);
+
+    // widgets.jsの設定
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+    tweetDivided.appendChild(script);
 };
 
 const answers = [
